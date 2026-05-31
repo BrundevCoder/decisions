5d91618fd2ff135a1223de861c7696f02914691b
BrundevCoder <brunodevdrums@gmail.com>
2026-05-31 21:40:14 +0100

    prepared js for API, finished html and css

---
+++ script.js
@@ -0,0 +1,38 @@
+const answerDisplay = document.getElementById("answer");
+const questionInput = document.getElementById("userQuestion");
+const getResponseBtn = document.getElementById("obtainAnswerBtn");
+
+const API_URL = "";
+
+function getAnswer() {
+  getResponseBtn.disabled = true;
+  getResponseBtn.innerText = "Getting your answer...";
+
+  fetch(API_URL)
+    .then(response => {
+
+      if (!response.ok) {
+        throw new Error("something went wrong");
+        return;
+      }
+
+      return response.json();
+    })
+    .then(answer => {
+      let actualAnswer = answer["answer"];
+
+      answerDisplay.innerText = actualAnswer;
+
+      getResponseBtn.disabled = false;
+    })
+    .catch(error => {
+      console.error(error);
+      answerDisplay.innerText = error;
+
+      getResponseBtn.disabled = false;
+    })
+
+  getResponseBtn.innerText = "Get Answered";
+}
+
+getResponseBtn.addEventListener("click", getAnswer);
